9884caeff080565ff282f7ef86c3d383e827283f
Kausiki <90125663+kausiki02@users.noreply.github.com>
2022-06-17 23:57:08 +0530

    Time: 1 ms (98.55%), Space: 44.1 MB (64.04%) - LeetHub

---
+++ 110-balanced-binary-tree/110-balanced-binary-tree.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        return dfsHeight(root) !=-1;
+        
+    }
+    
+    int dfsHeight(TreeNode root){
+        if(root == null) return 0;
+        
+        int leftHeight = dfsHeight(root.left);
+        if(leftHeight == -1) return -1;
+        int rightHeight = dfsHeight(root.right);
+        if(rightHeight == -1) return -1;
+        
+        if(Math.abs(leftHeight-rightHeight)>1) return -1; //if this statement is false that means not a balanced BT therefore will return the height of the binary tree.
+        
+        
+        return 1+ Math.max(leftHeight, rightHeight);
+    }
+}
