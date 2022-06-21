ae7c6d5f185d1f14ea7905fdd3b1048efbd0b11f
Kausiki <90125663+kausiki02@users.noreply.github.com>
2022-06-22 00:10:50 +0530

    Time: 41 ms (19.33%), Space: 70.3 MB (75.67%) - LeetHub

--- 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.java
+++ 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.java
@@ -1,20 +1,36 @@
 class Solution {
     public int furthestBuilding(int[] heights, int bricks, int ladders) {
-        var n = heights.length;
-	var ladderClimbs = new PriorityQueue<Integer>();
+        
+/*Explanation && Idea:
+*Some observations:
 
-	for (var i = 1; i < n; i++) {
-		var climb = heights[i] - heights[i - 1];
-		if (climb <= 0)
-			continue; // descent, skip
-			
-		ladderClimbs.add(climb);
-		if (ladderClimbs.size() <= ladders)
-			continue; // use a ladder
-		
-		if ((bricks -= ladderClimbs.poll()) < 0)
-			return i - 1; // insufficient bricks, terminate
-	}
-	return n - 1;
+with ladder we can cover any height . So will Prefer ladder to cover large difference.
+If next building height is greater than only we need brick or ladder to cross it , if next building height if less than current building we can directly jump.
+*Algorithm:
+Go greedly
+
+First use all the ladders whenever we have to go to higher building and also add the difference b/w height in min_heap.
+if min_heap.size()==ladders it means we have used all the ladders.
+now let say after all ladders are used and we we found a larger building , add difference to min_heap and use bricks at a place where height difference is minimum by replacing the ladder from that place.*/
+        
+        //best and easiest sol
+        PriorityQueue<Integer> min_heap=new PriorityQueue<>();  
+          
+        for(int i=0;i<heights.length-1;i++){
+            int diff=heights[i+1]-heights[i];  // diff: difference
+            
+            if(diff>0){
+                min_heap.add(diff);
+            }
+            
+            if(min_heap.size()>ladders){
+                bricks-=min_heap.remove();
+            }
+            
+            if(bricks<0){  //means both bricks and ladder are finished .
+                return i;
+            }
+        }  
+       return heights.length-1;
 }
 }
