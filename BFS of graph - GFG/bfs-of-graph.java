4d21b46cd972427681345889566df038f5994e0d
Kausiki <90125663+kausiki02@users.noreply.github.com>
2022-06-28 13:12:22 +0530

    Added solution - LeetHub

--- BFS of graph - GFG/bfs-of-graph.java
+++ BFS of graph - GFG/bfs-of-graph.java
@@ -35,63 +35,33 @@ class GFG {
 class Solution {
     // Function to return Breadth First Traversal of given graph.
     public ArrayList<Integer> bfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj) {
-       /*
+       
         ArrayList<Integer> bfs = new ArrayList<>();
         boolean vis[] = new boolean[V];
+        Queue<Integer> q = new LinkedList<>();
         
-        for(int i=1; i<=V; i++){
-            if(vis[i] == false){
-                Queue<Integer> q = new LinkedList<>();
-                q.add(i);
-                vis[i] = true;
+        q.add(0);
+        vis[0] = true;
                 
                 //bfs
                 
-                while(!q.isEmpty()){
-                    Integer node = q.poll();
-                    bfs.add(node);
-                    
-                    for(Integer it: adj.get(node)){
-                        if(vis[it]==false){
-                            vis[it]=true;
-                            q.add(it);
+        while(!q.isEmpty()){
+            Integer node = q.poll();
+                bfs.add(node);
+                  
+            // Get all adjacent vertices of the dequeued vertex s
+            // If a adjacent has not been visited, then mark it
+            // visited and enqueue it  
+                for(Integer it: adj.get(node)){
+                    if(vis[it]==false){
+                        vis[it]=true;
+                        q.add(it);
                         }
                     }
                 }
                 
-            }
-        }
-        return bfs; */
+        return bfs; 
         
-        ArrayList < Integer > bfs = new ArrayList < > ();
-        boolean vis[] = new boolean[V];
-        Queue < Integer > q = new LinkedList < > ();
 
-        q.add(0);
-        vis[0] = true;
-
-        while (!q.isEmpty()) {
-            Integer node = q.poll();
-            bfs.add(node);
-
-            // Get all adjacent vertices of the dequeued vertex s
-            // If a adjacent has not been visited, then mark it
-            // visited and enqueue it
-            for (Integer it: adj.get(node)) {
-                if (vis[it] == false) {
-                    vis[it] = true;
-                    q.add(it);
-                }
-            }
-        }
-
-        return bfs;
-
-        
-        
-        
-        
-        
-        
     }
 }
