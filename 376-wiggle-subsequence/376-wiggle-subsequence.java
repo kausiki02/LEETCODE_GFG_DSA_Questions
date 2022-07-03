fe539b960a077f303710dcb258463ce9e33f2318
Kausiki <90125663+kausiki02@users.noreply.github.com>
2022-07-04 00:56:52 +0530

    Time: 0 ms (100.00%), Space: 42.2 MB (15.00%) - LeetHub

--- 376-wiggle-subsequence/376-wiggle-subsequence.java
+++ 376-wiggle-subsequence/376-wiggle-subsequence.java
@@ -1,19 +1,19 @@
 class Solution {
     public int wiggleMaxLength(int[] nums) {
-        // 3 possible condtions
-        // 1. a[i] > a[i-1] - up position and element before down position
-        // 2. a[i] < a[i-1] - down position and element before up position
-        // 3. a[i] == a[i-1]
-        int up = 1;
-        int down = 1;
-        for (int i=1; i<nums.length; i++) {
-            if (nums[i] > nums[i - 1]) {
-                up = down + 1;
-            }
-            else if (nums[i] < nums[i - 1]) {
-                down = up + 1;
+       if(nums.length == 1) return 1;
+        
+        int prevSign = nums[1] - nums[0];
+        int length = prevSign != 0 ? 2:1; //if the array has more than 2 elements the minimum subsequence count will be 2
+        
+        for(int i=2; i<nums.length; i++){
+            int currSign = nums[i] -nums[i-1];
+            if((currSign>0 && prevSign<=0)  || (currSign<0 && prevSign>=0)){
+                length++;
+                prevSign = currSign; //set the current sign as the previous's sign and move ahead
             }
         }
-        return Math.max(down, up);
+        return length;
+        
+        
     }
 }
