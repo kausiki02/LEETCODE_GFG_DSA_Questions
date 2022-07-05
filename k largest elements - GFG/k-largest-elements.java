591287b4d93fd9ba76a2007be262e1753f673fe6
Kausiki <90125663+kausiki02@users.noreply.github.com>
2022-07-06 00:53:50 +0530

    Added solution - LeetHub

---
+++ k largest elements - GFG/k-largest-elements.java
@@ -0,0 +1,56 @@
+// { Driver Code Starts
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            inputLine = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(inputLine[0]);
+            int k = Integer.parseInt(inputLine[1]);
+            int[] arr = new int[n];
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            int[] ans = new Solution().kLargest(arr, n, k);
+            for (int x : ans) {
+                System.out.print(x + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+    int[] kLargest(int[] arr, int n, int k) {
+        // code here
+        
+        PriorityQueue<Integer> pqmin = new PriorityQueue<Integer>();
+        
+        
+        
+        for(int i=0; i<n; i++){
+            pqmin.add(arr[i]);
+            
+            if(pqmin.size()>k){
+                pqmin.poll();
+            }
+        }
+        
+        int ans[] = new int[k];
+        int index=k-1;
+        while(!pqmin.isEmpty()){
+            ans[index--] = pqmin.poll();
+        }
+        
+        return ans;
+    }
+}
