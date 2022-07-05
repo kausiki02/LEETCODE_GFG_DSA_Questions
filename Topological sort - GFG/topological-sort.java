a3163909fe648562578b1085d64be5647efeb7b6
Kausiki <90125663+kausiki02@users.noreply.github.com>
2022-07-05 17:56:09 +0530

    Added solution - LeetHub

--- Topological sort - GFG/topological-sort.java
+++ Topological sort - GFG/topological-sort.java
@@ -59,31 +59,45 @@ class Main {
 
 class Solution
 {
-    static void findTopoSort(int node, int vis[],ArrayList<ArrayList<Integer>> adj,Stack<Integer> st){
-        vis[node] = 1;
-        for(Integer it: adj.get(node)){
-            if(vis[it]==0){
-                findTopoSort(it,vis,adj,st);
-            }
-        }
-        st.push(node);
-    } 
+     
     //Function to return list containing vertices in Topological order. 
     static int[] topoSort(int V, ArrayList<ArrayList<Integer>> adj) 
     {
-        Stack<Integer> st = new Stack<Integer>();
-        int vis[] = new int[V+1];
+        int topo[] = new int[V];
+        int indegree[] = new int[V];
         
-        for(int i=0; i<V; i++){
-            if(vis[i]==0){
-                findTopoSort(i,vis,adj,st);
+        //calculating indegrees
+        for(int i=0; i<V; i++)
+        {
+            for(Integer it: adj.get(i)){
+                indegree[it]++;
+            }
+    }
+    
+    //now taking a queue data structure
+    
+         Queue<Integer> q = new LinkedList<Integer>();
+        for(int i=0; i<V; i++)
+        {
+            if(indegree[i]==0){ //adding the elements in the stack which has indegree =0
+                q.add(i);
+        }
+        }
+        
+        //run the bfs algo
+        int ind=0;
+        while(!q.isEmpty()){
+            Integer node = q.poll();
+            topo[ind++] = node; // whenever we take out an element that gets added to the answer i.e topo sort
+        
+        //now we have to decrease the adjacent's nodes indegree by 1
+        for(Integer it: adj.get(node)){
+            indegree[it]--;
+            if(indegree[it]==0){ //and when the indegree of a element becomes 0 we add it to the queue
+                q.add(it);
             }
         }
         
-        int topo[] = new int[V];
-        int ind =0;
-        while(!st.isEmpty()){
-            topo[ind++] = st.pop();
         }
         return topo;
     }
