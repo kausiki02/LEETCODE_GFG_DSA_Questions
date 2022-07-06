ce78766b3e43956163ac9596a77bcd9fb824651a
Kausiki <90125663+kausiki02@users.noreply.github.com>
2022-07-07 00:27:45 +0530

    Time: 20 ms (45.60%), Space: 50.9 MB (26.77%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.java
@@ -7,18 +7,18 @@ class Solution {
              hm.put(nums[i], hm.getOrDefault(nums[i], 0) + 1);
         }
         
-        PriorityQueue<Integer> minHeap = new PriorityQueue<>((a, b) -> hm.get(b) - hm.get(a));
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>((a, b) -> hm.get(a) - hm.get(b));
         
         for(int key : hm.keySet()){
             minHeap.add(key);
             
-          /*  if(minHeap.size()>k){ // iski zaroorat nahi because tu niche last ke do elements hi le rhi heap se i<k (0-2)
+          if(minHeap.size()>k){ // iski zaroorat nahi because tu niche last ke do elements hi le rhi heap se i<k (0-2)
                 minHeap.poll();
-            } */
+            } 
         }
         int res[] = new int[k];
-        for(int i = 0; i < k; i++){
-            res[i] = minHeap.poll();
+        while(!minHeap.isEmpty()){
+            res[--k] = minHeap.poll();
         }
         return res;
     }
