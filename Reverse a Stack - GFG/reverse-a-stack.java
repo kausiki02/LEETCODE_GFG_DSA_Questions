22c17ae6ee6d884eff40c5ba081de02f64e4c87e
Kausiki <90125663+kausiki02@users.noreply.github.com>
2022-07-13 00:28:25 +0530

    Added solution - LeetHub

---
+++ Reverse a Stack - GFG/reverse-a-stack.java
@@ -0,0 +1,67 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+        
+        //taking testcases
+        int t = Integer.parseInt(br.readLine()); 
+    	for(int i=0;i<t;i++)
+    	{
+            String str=br.readLine();
+    		
+    		//input n and d
+    	    int n=Integer.parseInt(str);
+    		Stack<Integer> stack=new Stack<>();
+    		String str1=br.readLine();
+    		String[] starr1=str1.split(" ");
+    		
+    		//inserting elements in the array
+    		for(int j=0;j<n;j++)
+    		{
+    		  stack.push(Integer.parseInt(starr1[j]));
+    		}
+    		//calling rotateArr() function
+            ArrayList<Integer> res=Solution.reverse(stack);
+            for(int ii=0;ii<res.size();ii++){
+                System.out.print(res.get(ii));
+                if(ii!=res.size()-1){
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+         }
+    }
+}// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{ 
+    static ArrayList<Integer> reverse(Stack<Integer> s)
+    {
+        // add your code here
+        ArrayList<Integer> res = new ArrayList<Integer>();
+        reverse(s,res);
+        return res;
+        
+    }
+    
+    static void reverse(Stack<Integer>s, ArrayList<Integer> arr){
+        if(s.size()==0)
+            return;
+            
+        int temp = s.peek();
+        arr.add(temp);
+        s.pop();
+        reverse(s,arr);
+    }
+}
