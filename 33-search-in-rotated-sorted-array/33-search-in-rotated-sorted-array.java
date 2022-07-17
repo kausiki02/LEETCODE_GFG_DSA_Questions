f4a39e1e96ff26d3931d792012d01a79cfb4ed15
Kausiki <90125663+kausiki02@users.noreply.github.com>
2022-07-18 01:28:18 +0530

    Time: 1 ms (74.47%), Space: 43.2 MB (9.62%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
@@ -1,9 +1,33 @@
 class Solution {
     public int search(int[] nums, int target) {
-        //linear search
-        for(int i=0; i<nums.length; i++){
-            if(nums[i]==target){
-                return i;
+        //binary search
+        
+        int low = 0; int high = nums.length-1;
+        
+        while(low<=high){
+             int mid = (low+high)/2;
+             if(nums[mid] == target){
+                 return mid;
+             }
+            
+            //check if the left side is sorted or not
+            if(nums[low]<=nums[mid]){
+                //figure out if your element lies on the left half or not
+                if(nums[low]<=target && target<=nums[mid]){
+                    high = mid -1;
+                }
+                else{
+                    low = mid+1;
+                }
+            }
+            //if left half not sorted then right half is sorted 
+            else{
+                if(target>= nums[mid] && target<= nums[high]){
+                    low = mid+1;
+                }
+                else{
+                    high = mid-1;
+                }
             }
         }
         return -1;
