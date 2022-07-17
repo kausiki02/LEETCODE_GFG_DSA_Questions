cf9b8a91fc0d202f719983359820b98052b93fff
Kausiki <90125663+kausiki02@users.noreply.github.com>
2022-07-18 00:47:45 +0530

    Time: 0 ms (100.00%), Space: 41.8 MB (90.23%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
@@ -1,5 +1,6 @@
 class Solution {
     public int search(int[] nums, int target) {
+        //linear search
         for(int i=0; i<nums.length; i++){
             if(nums[i]==target){
                 return i;
