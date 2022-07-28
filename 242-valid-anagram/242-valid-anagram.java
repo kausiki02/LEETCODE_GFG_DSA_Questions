ff785bed136c3e300b9906fe08f653348266a377
Kausiki <90125663+kausiki02@users.noreply.github.com>
2022-07-28 12:45:33 +0530

    Time: 5 ms (75.47%), Space: 43.8 MB (57.92%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.java
+++ 242-valid-anagram/242-valid-anagram.java
@@ -1,35 +1,33 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-      /*  //if(s.length()!=t.length()) return false;
-        
-        int[] char_counts = new int[26];
-        
-        for(int i=0; i<s.length(); i++){
-            
-            char_counts[s.charAt(i)-'a']++; 
+         
             
             // this takes current character from each string subtracts
             //a to get index of that current character in the alphabet 
             //and then it increments for the above.
-        }
-            
-        for(int i=0; i<t.length(); i++){ 
-            char_counts[s.charAt(i)-'a']--; // and decrements for this   
+    
+        
+        if(s.length()!=t.length()){
+            return false;
         }
         
-        for(int i: char_counts){
+        int char_counts[] = new int[26];
+        
+        for(int i=0; i<s.length(); i++){
+            char_counts[s.charAt(i) - 'a']++;
+            char_counts[t.charAt(i) - 'a']--;
+        }
+        
+        for(int i : char_counts){
             if(i!=0){
                 return false;
             }
         }
-        return true; */
-        
-        
-        int[] alphabet = new int[26];
-        for (int i = 0; i < s.length(); i++) alphabet[s.charAt(i) - 'a']++;
-        for (int i = 0; i < t.length(); i++) alphabet[t.charAt(i) - 'a']--;
-        for (int i : alphabet) if (i != 0) return false;
         return true;
+        
+        
+            
+       
         
         
         
